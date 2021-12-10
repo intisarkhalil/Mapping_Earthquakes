a0b9681635808734825555a8f95fa0972cc709e4
intisar <62036983+intisarkhalil@users.noreply.github.com>
2021-12-10 08:49:13 -0700

    update files and foldres

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -3,7 +3,39 @@
 let map = L.map("mapid").setView([34.0522, -118.2437], 14);
 
 //  Add a marker to the map for Los Angeles, California.
-let marker = L.circleMarker([34.0522, -118.2437], {
+// An array containing each city's location, state, and population.
+let cities = [{
+  location: [40.7128, -74.0059],
+  city: "New York City",
+  state: "NY",
+  population: 8398748
+},
+{
+  location: [41.8781, -87.6298],
+  city: "Chicago",
+  state: "IL",
+  population: 2705994
+},
+{
+  location: [29.7604, -95.3698],
+  city: "Houston",
+  state: "TX",
+  population: 2325502
+},
+{
+  location: [34.0522, -118.2437],
+  city: "Los Angeles",
+  state: "CA",
+  population: 3990456
+},
+{
+  location: [33.4484, -112.0740],
+  city: "Phoenix",
+  state: "AZ",
+  population: 1660272
+}
+];
+{
   radius: 300,
   color: "black",
   fillcolor:"#ffffall"
@@ -14,6 +46,5 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tile
     maxZoom: 18,
     accessToken: API_KEY
 });
-
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
