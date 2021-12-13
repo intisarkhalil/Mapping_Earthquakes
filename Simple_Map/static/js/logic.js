31e83656ccdbfcdbdb1edb933b12105559bcedac
intisar <62036983+intisarkhalil@users.noreply.github.com>
2021-12-13 10:09:12 -0700

    edit

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -16,7 +16,6 @@ L.polyline(line, {
   weight:4,
   opasity:0.5
 }).addTo(map);
-
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
