9bcdbc99b1244316320a46bded39a4ad0091d280
intisar <62036983+intisarkhalil@users.noreply.github.com>
2021-12-13 19:51:13 -0700

    mapping geojson polygons

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -1,19 +1,56 @@
 // Create the map object with a center and zoom level.
 
-let map = L.map("mapid").setView([34.0522, -118.2437], 14);
+//let map = L.map("mapid").setView([34.0522, -118.2437], 14);
 
 //  Add a marker to the map for Los Angeles, California.
-let marker = L.circleMarker([34.0522, -118.2437], {
-  radius: 300,
-  color: "black",
-  fillcolor:"#ffffall"
-}).addTo(map);
+//let marker = L.circleMarker([34.0522, -118.2437], {
+  //radius: 300,
+  //color: "black",
+ // fillcolor:"#ffffall"
+//}).addTo(map);
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
 });
 
 // Then we add our 'graymap' tile layer to the map.
-streets.addTo(map);
+//streets.addTo(map);
+// We create the tile layer that will be the background of our map.
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+// Create a base layer that holds both maps.
+let baseMaps = {
+  "Streets": streets,
+  "Satwllite Streets": satelliteStreets
+};
+// Create the map object with center, zoom level and default layer.
+let map = L.map('mapid', {
+  center: [43.7, -79.3],
+  zoom: 11,
+  layers: [satelliteStreets]
+})
+// Pass our map layers into our layers control and add the layers control to the map.
+L.control.layers(baseMaps).addTo(map);
+// Accessing the airport GeoJSON URL
+let torontoHoods = "https://raw.githubusercontent.com/intisarkhalil/Mapping_Earthquakes/main/torontoNeighborhoods.json";
+// create style
+let myStyle= {
+  color: "blue",
+  weight: 1,
+  fillColor:"yellow"
+}
+// Grabbing our GeoJSON data.
+d3.json(torontoHoods).then(function(data) {
+  console.log(data);
+// Creating a geoJSON layer with the retrieved data
+L.geoJson(data, {
+  style: myStyle,
+  onEachFeature:function(feature, layer) {
+    layer.bindPopup("<h3>Neighborhood:" + feature.properties.AREA_NAME + "</h3>")}
+}).addTo(map);
+});
